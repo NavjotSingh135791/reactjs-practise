6d8d75f52a9d5f5707d64485671be83b3694ec38
NavjotSingh135791 <116021714+NavjotSingh135791@users.noreply.github.com>
2022-10-18 18:21:01 +0530

    create counter increment decrement UI

--- components/counter.jsx
+++ components/counter.jsx
@@ -3,8 +3,12 @@ import {useState} from react;
 function Counter() {
   const [counter, setCounter] = useState(0);
   return (
+    <>
     <div>
       Counter: {counter}
     </div>
+    <div onClick={()=>setCounter(old=>old+1)}>+</div>
+    <div onClick={()=>setCounter(old=>old-1)}>-</div>
+    </>
   )
 }
